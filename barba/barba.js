479c8d97bc61f3796883f8f21b69b69f890c0ce3
kes3583 <kes3583@gmail.com>
2018-08-30 08:43:59 +0200

    index.js

--- barba/barba.js
+++ barba/barba.js
@@ -1,3 +1,22 @@
 $('document').ready(function(){
-     Barba.Pjax.start();
-});
+           var transEffect = Barba.BaseTransition.extend({
+               start: function(){
+                 this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
+               },
+               fadeInNewcontent: function(nc) {
+                 nc.hide();
+                 var _this = this;
+                 $(this.oldContainer).fadeOut(1000).promise().done(() => {
+                   nc.css('visibility','visible');
+                   nc.fadeIn(1000, function(){
+                     _this.done();
+                   })
+                 });
+               }
+           });
+
+           Barba.Pjax.getTransition = function() {
+             return transEffect;
+           }
+           Barba.Pjax.start();
+         });
