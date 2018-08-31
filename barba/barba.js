a7c62101733e77d678bdd44951551f2e02f98899
kes3583 <kes3583@gmail.com>
2018-08-31 10:50:07 +0200

    examing

--- barba/barba.js
+++ barba/barba.js
@@ -4,6 +4,7 @@ $('document').ready(function(){
                  this.newContainerLoading.then(val => this.fadeInNewcontent($(this.newContainer)));
                },
                fadeInNewcontent: function(nc) {
+                 console.log(nc);
                  nc.hide();
                  var _this = this;
                  $(this.oldContainer).fadeOut(1000).promise().done(() => {
